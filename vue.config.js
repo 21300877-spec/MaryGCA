f568bf1fa93d5f5a3ae2bd3261ade6ecfbb757f5
21300877-spec <21300877@uttt.edu.mx>
2026-04-18 02:09:27 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   devServer: {
     proxy: {
+      // Este proxy SOLO funciona en localhost
       '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/auth': {
         target: 'http://localhost:8081',
         changeOrigin: true,
         secure: false,
